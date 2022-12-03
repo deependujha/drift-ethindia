19908501f2df7c13014970f3018b4ca7115c7d3f
Niteshkumar <nkumar35101@gmail.com>
2022-12-03 13:12:09 +0530

    input field fixed

--- frontend/components/chatroom/Chats.jsx
+++ frontend/components/chatroom/Chats.jsx
@@ -1,86 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import socketIO from 'socket.io-client';
-import ChatCard from '../chatCard/ChatCardLeft';
-import axios from 'axios';
-const ENDPOINT = 'http://localhost:4500';
-const socket = socketIO(ENDPOINT, { transport: ['websocket'] });
+import React, { useState, useEffect } from "react";
+import socketIO from "socket.io-client";
+import ChatCard from "../chatCard/ChatCardLeft";
+import styles from "../../styles/Chats.module.css"
+import axios from "axios";
+const ENDPOINT = "http://localhost:4500";
+const socket = socketIO(ENDPOINT, { transport: ["websocket"] });
 // the axios instance with the api endpoint
 const axiosInstance = axios.create({
-	baseURL: 'http://localhost:4500',
+  baseURL: "http://localhost:4500",
 });
 
 const Chats = ({ currLeft }) => {
-	const [room, setRoom] = useState('');
-	const [msgs, setMsgs] = useState([]);
-	const [txt, setTxt] = useState('');
+  const [room, setRoom] = useState("");
+  const [msgs, setMsgs] = useState([]);
+  const [txt, setTxt] = useState("");
 
-	socket.on('connect', () => {
-		// console.log('new connection frontend'); // ojIckSD2jqNzOqIrAGzL
-		// console.log(`socket.id: ${socket.id}`);
-	});
+  socket.on("connect", () => {
+    // console.log('new connection frontend'); // ojIckSD2jqNzOqIrAGzL
+    // console.log(`socket.id: ${socket.id}`);
+  });
 
-	socket.emit('join_room', currLeft);
+  socket.emit("join_room", currLeft);
 
-	useEffect(() => {
-		socket.on('receive_message', (data) => {
-			if (data.room === currLeft) setMsgs((msgs) => [...msgs, data]);
-		});
-	}, [socket]);
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      if (data.room === currLeft) setMsgs((msgs) => [...msgs, data]);
+    });
+  }, [socket]);
 
-	useEffect(() => {
-		fetchData();
-	}, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-	const fetchData = () => {
-		axiosInstance
-			.get(`/${currLeft}`)
-			.then((resp) => {
-				console.log('resp: ', resp);
-				setMsgs(resp.data);
-			})
-			.catch((err) => {
-				console.log('error occurred: ', err);
-			});
-	};
+  const fetchData = () => {
+    axiosInstance
+      .get(`/${currLeft}`)
+      .then((resp) => {
+        console.log("resp: ", resp);
+        setMsgs(resp.data);
+      })
+      .catch((err) => {
+        console.log("error occurred: ", err);
+      });
+  };
 
-	const sendMsg = async () => {
-		if (txt === '') {
-			return;
-		}
-		socket.emit('send_message', {
-			room: currLeft,
-			msg: txt,
-			sender: 'deependu',
-		});
-		setMsgs([...msgs, { room: currLeft, msg: txt, sender: 'deependu' }]);
-		setTxt('');
-	};
+  const sendMsg = async () => {
+    if (txt === "") {
+      return;
+    }
+    socket.emit("send_message", {
+      room: currLeft,
+      msg: txt,
+      sender: "deependu",
+    });
+    setMsgs([...msgs, { room: currLeft, msg: txt, sender: "deependu" }]);
+    setTxt("");
+  };
 
-	return (
-		<div>
-			Chats
-			<div>
-				{msgs.map((ms, idx) => (
-					<div key={idx}>
-						<ChatCard sender={ms.sender} msg={ms.msg} />
-					</div>
-				))}
-			</div>
-			<div className="d-flex">
-				<input
-					type="email"
-					value={txt}
-					onChange={(e) => setTxt(e.target.value)}
-					className="form-control mx-2 my-2"
-					placeholder="Type your message..."
-				/>
-				<button
-					onClick={sendMsg}
-					className="btn btn-success mx-2 my-2 px-3"
-				>{`>`}</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="">
+      Chats
+      <div>
+        {msgs.map((ms, idx) => (
+          <div key={idx}>
+            <ChatCard sender={ms.sender} msg={ms.msg} />
+          </div>
+        ))}
+      </div>
+      <div className="position-absolute bottom-0" style={{width:"750px"}}>
+        <div className="d-flex">
+          <input
+            type="email"
+            value={txt}
+            onChange={(e) => setTxt(e.target.value)}
+            className="form-control mx-2 my-2"
+            placeholder="Type your message..."
+          />
+          <button
+            onClick={sendMsg}
+            className="btn btn-success mx-2 my-2 px-3"
+          >{`>`}</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
