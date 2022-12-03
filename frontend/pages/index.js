7fcd3cd687895cac7cdd06cb4deff5ce2521a466
Niteshkumar <nkumar35101@gmail.com>
2022-12-03 18:38:42 +0530

    landing page finished

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,28 +6,37 @@ import Button from "react-bootstrap/Button";
 
 function landing() {
   return (
-    <Row className="bg-secondary" style={{ height: "99vh" }}>
+    <Row
+      className="bg-info text-white"
+      style={{ height: "100vh", width: "100vw" }}
+    >
       <Col sm={6} className="text-center my-auto">
         <div>
-          <Button variant="success">
-            <Image src="/metamask_landing_logo.png" height="35" width="35" />{" "}
-            Connect With MetaMask
-          </Button>
+          <p class="fs-1 fw-bold text-dark">Get Updated With Your DAO</p>
         </div>
-        <div className="mt-3">
-          <Button variant="success">
-            {" "}
-            <Image
-              src="/lens-protocol-landing.jpg"
-              height="35"
-              width="35"
-            />{" "}
-            Connect With Lens_ID
-          </Button>
-        </div>
+        <Button variant="success" className="me-3">
+          <Image
+            alt="loading..."
+            src="/metamask_landing_logo.png"
+            height="35"
+            width="35"
+          />
+          Connect With MetaMask
+        </Button>
+        <Button variant="success">
+          {" "}
+          <Image
+            alt="loading..."
+            src="/lense_landing.jpg"
+            height="35"
+            width="35"
+            className="rounded-5"
+          />{" "}
+          Connect With Lens_ID
+        </Button>
       </Col>
       <Col sm={6} className="my-auto">
-        <Image src="/daooo.png" height="537" width="600" alt="loading..." />
+        <Image src="/landing.png" height="537" width="600" alt="loading..." />
       </Col>
     </Row>
   );
