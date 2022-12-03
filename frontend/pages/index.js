56415f8f33c01ec7c86d5fea379f689c4cb242d5
deependujha <deependujha21@gmail.com>
2022-12-03 09:50:05 +0530

    index.js page cleared

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,11 @@
-import Image from 'next/image'
-import ChatRoom from '../components/chatroom/DashBoard'
-import styles from '../styles/Home.module.css'
-import Container from "react-bootstrap/Container";
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Container from 'react-bootstrap/Container';
 
 export default function Home() {
-  return (
-    <Container style={{height: "100vh"}}>
-      <ChatRoom/>
-    </Container>
-  )
+	return (
+		<Container style={{ height: '100vh' }}>
+			<div>index.js page</div>
+		</Container>
+	);
 }
